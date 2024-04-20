da004de6a1029fa68bf0bb81497902f2a39b6cf1
alirazarao21 <raoa62253@gmail.com>
2024-04-20 13:13:26 +0500

    conersion

---
+++ Convesion.js
@@ -0,0 +1,11 @@
+// let score = 71
+// let score = null
+// let score = undefined
+let score = true 
+// let score = "Ali raza rao" this string can not b converted in number s its type will bw NaN 
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valInString = Number(score)
+console.log(typeof valInString);
+console.log(valInString);
