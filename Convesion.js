e9b0370d61701d669ab285838ef489273b1a76aa
alirazarao21 <raoa62253@gmail.com>
2024-04-22 11:08:04 +0500

    prefix and postfix increment

--- Convesion.js
+++ Convesion.js
@@ -7,5 +7,39 @@ let score = true
 // console.log(typeof(score));
 
 let valInString = Number(score)
-console.log(typeof valInString);
-console.log(valInString);
+// console.log(typeof valInString);
+// console.log(valInString);
+
+
+//****************************************************Operations */
+
+let value = 7
+let anotherValue = -value
+// console.log(anotherValue);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log( "1" + "2");  
+// console.log(1 + 2 +"2");
+
+// the upper comments are soo confusing that they vary in their out put soo dont depend on them .Instead use brackets to use operation to b performed accordingly
+
+
+let nuum1, num2, num=
+num1= num2= num3 = 2 + 3
+// console.log(num3);
+
+
+/************************************  PREFIX AND POSTFIX INCREMENTS */
+
+let x = 3
+const y = x++
+
+console.log(`x: ${x}, y:${y}`);
+
+
+let a = 3
+const b = ++a
+
+console.log(`a: ${a}, b:${b}`);
+  
