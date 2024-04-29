7455d5cbff7c301002144bde2324c7e3bc41d376
alirazarao21 <raoa62253@gmail.com>
2024-04-29 14:57:30 +0500

    Maths and Nums

---
+++ NumsAndMaths.js
@@ -0,0 +1,36 @@
+const score = 100 
+//  console.log(typeof score);
+const Balance = new Number(score)
+
+// console.log(Balance);
+// console.log(Balance.toString());
+// console.log(typeof Balance);
+
+const otherNumber = 123.45345
+// console.log(otherNumber.toFixed(3));
+// console.log(otherNumber.toPrecision(4));
+
+const newNum = 10000000
+// console.log(newNum.toLocaleString('en-IN'));
+
+
+/**************************Maths ********************************************* */
+//  console.log(Math.abs(-44));
+
+//  console.log(Math.round(5.44));
+//  console.log(Math.ceil(5.44));
+//  console.log(Math.floor(5.44));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10 
+const max = 20
+ 
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
