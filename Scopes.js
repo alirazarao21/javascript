4d96d534c86d974131a8247ade585016e2bfdb42
alirazarao21 <raoa62253@gmail.com>
2024-05-02 20:15:33 +0500

    scopes in js

---
+++ Scopes.js
@@ -0,0 +1,33 @@
+function one (){
+    const username ="Alirazarao"
+
+    function two() {
+        const website = "Youtube"
+        console.log(website);
+
+        console.log(username);
+    }
+    two()
+}
+// one()
+
+if (true) {
+  const  intro = "Razi"
+
+    if (intro === "Razi") {
+        const channel = " Youtube"
+        console.log(intro + channel);
+    }
+    // console.log(channel);
+}
+
+function addone (num){
+    return num + 1
+}
+addone(5)    // normal funtions 
+
+
+const addtwo = function (num){
+    return num + 2
+}
+addtwo(5)  // this is called expression , agr ksi fucntion ko vairable ma hold kro gaye to wo expression kehlata hai
