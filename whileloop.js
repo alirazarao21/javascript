8436fa91ba4c2ba71bf4c6f2a85a5b222a0dc488
alirazarao21 <raoa62253@gmail.com>
2024-05-05 01:20:36 +0500

    while and do- while loops

---
+++ whileloop.js
@@ -0,0 +1,37 @@
+// while loop if condtion ki trah ka krta hai ... lekin is ma index ko pehly declare krna parta hai
+let index = 0
+
+while (index <=10) {
+   // console.log(`value of i is ${index}`);
+    index = index +3
+}
+
+let myarray =["Ali","Babar","hamza","usama","Tehseen"]
+let arr = 0
+while (arr <myarray.length) {
+//console.log(`fab 4 consists of ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+// do while loop 
+
+let score = 0
+
+do {
+    // console.log(`score is ${score}`);
+    score ++
+} while (score <=10);
+
+
+
+let scor = 11
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <=10);
+
+// what if our give score value is greater than give condition .. do-while loop ma loop ak dafa run hota hai us k bad condition check hoti hai
+// agr to condtion true ni hai to loop terminate ho jata hai, or agr true hai to give lenght tk chalta hai 
+
+
